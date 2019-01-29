f3dac1069ae06574aed074eb79a64402c8d75820
bigmorebig <1374265193@qq.com>
2019-01-29 10:33:45 +0800

    1/29

--- OnlineStudy/apps/users/views.py
+++ OnlineStudy/apps/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate,login
 
 # Create your views here.
+
+
 def user_login(request):
     if request.method == 'POST':
         user_name = request.POST.get('username','')
